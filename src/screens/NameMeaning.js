5b7210c8dd1904264b50478f3028d632cf01cdcb
Aarjav Jain <aarjavjain2309@gmail.com>
2023-06-14 17:07:05 +0530

    name meaning design i=0

--- src/screens/NameMeaning.js
+++ src/screens/NameMeaning.js
@@ -1,19 +1,62 @@
-import {View} from 'react-native';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
 import React from 'react';
 import CustomText from '../components/common/CustomText';
+import {useTranslation} from 'react-i18next';
 
 export default function NameMeaning({route}) {
   const {data} = route.params;
+  const {t} = useTranslation();
+
   return (
-    <View>
-      <View>
-        <CustomText text="Name: " />
-        <CustomText text={data.name} />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.nameContainer}>
+        <CustomText
+          text={`${t('Name')}: `}
+          fontColor="white"
+          size={35}
+          weight="bold"
+        />
+        <CustomText text={data.name} fontColor="white" size={28} />
       </View>
-      <View>
-        <CustomText text="Meaning: " />
-        <CustomText text={data.meaning} />
+      <View style={styles.divider} />
+      <View style={styles.meaningContainer}>
+        <CustomText
+          text={`${t('Meaning')}: `}
+          fontColor="white"
+          size={35}
+          weight="bold"
+        />
+        <CustomText
+          text={data.meaning}
+          fontColor="white"
+          size={28}
+          textAlignment="justify"
+        />
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2196F3',
+    justifyContent: 'center',
+    gap: 20,
+    // alignItems: 'center',
+    padding: 20,
+  },
+  nameContainer: {
+    flexDirection: 'row',
+    // justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  divider: {
+    backgroundColor: 'white',
+    height: 5,
+  },
+  meaningContainer: {
+    justifyContent: 'center',
+  },
+});
