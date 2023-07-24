905d0a60772230ce7599e1db89af5aed7c13d3bd
Aarjav Jain <aarjavjain2309@gmail.com>
2023-07-24 16:21:55 +0530

    direct contact

---
+++ src/components/PhoneNo.js
@@ -0,0 +1,74 @@
+import {View, Pressable, Linking, StyleSheet, Image} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import icons from '../constants/icons';
+import CustomText from './common/CustomText';
+
+const PhoneNo = ({number}) => (
+  <View style={styles.phoneBox}>
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#2196F3',
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+        borderRadius: 8,
+        gap: 5,
+      }}>
+      <CustomText
+        text="Click Here"
+        size={20}
+        fontColor="white"
+        letterSpacing={0.7}
+        fontFamily="sans-serif-light"
+      />
+      <Feather name="arrow-right-circle" color="white" size={22} />
+    </View>
+    <Pressable
+      style={styles.phoneNo}
+      onPress={() => {
+        Linking.openURL('whatsapp://send?text=' + `&phone=91${number}`)
+          .then(() => {
+            console.log('WhatsApp Opened for ' + `${number}`);
+          })
+          .catch(() => {
+            alert('Make sure WhatsApp installed on your device');
+          });
+      }}>
+      <Image
+        source={icons.whatsapp}
+        style={{
+          width: 35,
+          height: 35,
+        }}
+      />
+      <CustomText
+        text={number}
+        size={20}
+        letterSpacing={0.5}
+        fontFamily="sans-serif-medium"
+      />
+    </Pressable>
+  </View>
+);
+
+export default PhoneNo;
+
+const styles = StyleSheet.create({
+  phoneBox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  phoneNo: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+    borderColor: 'green',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+  },
+});
--- src/screens/DirectContact.js
+++ src/screens/DirectContact.js
@@ -1,11 +1,19 @@
 import {View, SafeAreaView, StyleSheet} from 'react-native';
 import React from 'react';
 import CustomText from '../components/common/CustomText';
+import {useTranslation} from 'react-i18next';
+import PhoneNo from '../components/PhoneNo';
 
 export default function DirectContact() {
+  const {t} = useTranslation();
+
   return (
     <SafeAreaView style={styles.container}>
-      <CustomText text="Direct Contact" />
+      <CustomText text={t('Direct Contact Msg')} size={30} />
+      <View style={styles.whtsapDiv}>
+        <PhoneNo number="8948840344" />
+        <PhoneNo number="8949988068" />
+      </View>
     </SafeAreaView>
   );
 }
@@ -14,5 +22,11 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
     flex: 1,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+  },
+  whtsapDiv: {
+    gap: 20,
+    marginVertical: 30,
   },
 });
