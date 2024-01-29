b82adf4ee11eddff9aa744a14be116c0f30fc3e7
Aarjav Jain <aarjavjain2309@gmail.com>
2024-01-30 00:21:12 +0530

    added firebase for info

--- src/screens/DirectContact.js
+++ src/screens/DirectContact.js
@@ -1,18 +1,50 @@
-import {View, SafeAreaView, StyleSheet} from 'react-native';
+import {View, SafeAreaView, StyleSheet, ActivityIndicator} from 'react-native';
 import React from 'react';
 import CustomText from '../components/common/CustomText';
 import {useTranslation} from 'react-i18next';
 import PhoneNo from '../components/PhoneNo';
+import useFirebaseData from '../hooks/useFirebaseData';
 
 export default function DirectContact() {
   const {t} = useTranslation();
+  const {data, loading, error, netState} = useFirebaseData('data', 'Phone Nos');
+
+  if (netState.isConnected != null && !netState.isConnected) {
+    // console.log('line 77: ', netState.isConnected);
+    return (
+      <SafeAreaView
+        style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <CustomText text={t('No Internet')} size={18} fontColor="black" />
+      </SafeAreaView>
+    );
+  }
+
+  if (loading)
+    return (
+      <SafeAreaView
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <ActivityIndicator size={50} />
+      </SafeAreaView>
+    );
+
+  if (error)
+    return (
+      <SafeAreaView>
+        <CustomText text={error} fontColor="black" size={16} />
+      </SafeAreaView>
+    );
 
   return (
     <SafeAreaView style={styles.container}>
       <CustomText text={t('Direct Contact Msg')} size={30} />
       <View style={styles.whtsapDiv}>
-        <PhoneNo number="8948840344" />
-        <PhoneNo number="8949988068" />
+        {Object.values(data).map((phoneNo, index) => (
+          <PhoneNo key={index} number={phoneNo} />
+        ))}
       </View>
     </SafeAreaView>
   );
--- src/screens/Information.js
+++ src/screens/Information.js
@@ -1,13 +1,65 @@
-import {SafeAreaView, StyleSheet, View} from 'react-native';
-import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
+import React, {useEffect} from 'react';
 import CustomText from '../components/common/CustomText';
 import {useTranslation} from 'react-i18next';
+import useFirebaseData from '../hooks/useFirebaseData';
+import {LANGUAGES} from '../constants/consts';
 
 export default function Information() {
-  const {t} = useTranslation();
+  const {t, i18n} = useTranslation();
+  const curLanguage = i18n.language;
+  const {data, loading, error, netState} = useFirebaseData(
+    'data',
+    'Information',
+  );
+
+  let language =
+    curLanguage == LANGUAGES.ENGLISH.key
+      ? LANGUAGES.ENGLISH.label
+      : LANGUAGES.HINDI.label;
+
+  if (netState.isConnected != null && !netState.isConnected) {
+    // console.log('line 77: ', netState.isConnected);
+    return (
+      <SafeAreaView
+        style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <CustomText text={t('No Internet')} size={18} fontColor="black" />
+      </SafeAreaView>
+    );
+  }
+
+  if (loading)
+    return (
+      <SafeAreaView
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <ActivityIndicator size={50} />
+      </SafeAreaView>
+    );
+
+  if (error)
+    return (
+      <SafeAreaView>
+        <CustomText text={error} fontColor="black" size={16} />
+      </SafeAreaView>
+    );
+
   return (
     <SafeAreaView style={styles.container}>
-      <CustomText text={t('Info Msg')} size={28} />
+      <FlatList
+        data={data[language]}
+        keyExtractor={(item, index) => index}
+        renderItem={({item}) => <CustomText text={item} size={28} />}
+      />
+      {/* <CustomText text={t('Info Msg')} size={28} /> */}
     </SafeAreaView>
   );
 }
