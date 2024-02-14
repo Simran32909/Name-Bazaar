cf6ec3bd48c4b55f943ed5ac7e7af47c4d13ef91
Aarjav Jain <aarjavjain2309@gmail.com>
2024-02-15 04:40:06 +0530

    added rashi and nakshatras to firebase

--- src/hooks/useFirebaseData.js
+++ src/hooks/useFirebaseData.js
@@ -1,28 +1,18 @@
-import React, {useState, useEffect} from 'react';
-import {doc, getDoc} from 'firebase/firestore';
+import {useState, useEffect} from 'react';
+import {collection, doc, getDoc, getDocs} from 'firebase/firestore';
 import {db} from '../firebase/firebase';
-import {SELECTIONS} from '../constants/consts';
-import {LANGUAGES} from '../constants/consts';
-import {useTranslation} from 'react-i18next';
 import {useNetInfo} from '@react-native-community/netinfo';
 
-const useFirebaseData = (collection, document) => {
+const useFirebaseData = (collectionName, document = '') => {
   const netState = useNetInfo();
-  const {t, i18n} = useTranslation();
 
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const curLanguage = i18n.language;
-  let language =
-    curLanguage == LANGUAGES.ENGLISH.key
-      ? LANGUAGES.ENGLISH.label
-      : LANGUAGES.HINDI.label;
-
-  const fetchData = async (collection, document) => {
+  const fetchData = async (collectionName, document) => {
     try {
-      const docRef = doc(db, collection, document);
+      const docRef = doc(db, collectionName, document);
       const docSnap = await getDoc(docRef);
 
       if (docSnap.exists()) setData(docSnap.data());
@@ -36,92 +26,30 @@ const useFirebaseData = (collection, document) => {
     }
   };
 
+  const fetchAllDocuments = async collectionName => {
+    try {
+      const querySnapshot = await getDocs(collection(db, collectionName));
+      let obj = {};
+      querySnapshot.forEach((doc, index) => {
+        obj = {...obj, [doc.id]: doc.data()};
+      });
+      // console.log(querySnapshot);
+      setData(obj);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     setLoading(true);
 
     if (netState.isConnected) {
-      // console.log('line 67:', netState.isConnected);
-      // if (isStaticData) {
-      //   fetchStaticData(selection);
-      // } else if (!isUnique) {
-      //   if (selection == SELECTIONS.BOY) fetchData('boys');
-      //   else if (selection == SELECTIONS.GIRL) fetchData('girls');
-      // } else {
-      //   if (selection == SELECTIONS.BOY) fetchUniqueData('boys');
-      //   else if (selection == SELECTIONS.GIRL) fetchUniqueData('girls');
-      // }
-      fetchData(collection, document);
+      if (document) fetchData(collectionName, document);
+      else fetchAllDocuments(collectionName);
     }
-  }, [netState.isConnected, collection, document]);
-
-  // const fetchData = async collection => {
-  //   try {
-  //     const docRef = doc(db, collection, language);
-  //     const docSnap = await getDoc(docRef);
-
-  //     if (docSnap.exists()) setData(docSnap.data());
-  //     else console.log('No such document!');
-
-  //     setLoading(false);
-  //   } catch (error) {
-  //     // console.log(error);
-  //     setError(error);
-  //     setLoading(false);
-  //   }
-  // };
-
-  // const fetchUniqueData = async document => {
-  //   try {
-  //     const docRef = doc(db, 'unique names', document);
-  //     const docSnap = await getDoc(docRef);
-
-  //     if (docSnap.exists()) setData(docSnap.data()[language]);
-  //     else console.log('No such document!');
-
-  //     setLoading(false);
-  //   } catch (error) {
-  //     console.log(error);
-  //     setError(error);
-  //     setLoading(false);
-  //   }
-  // };
-
-  // const fetchStaticData = async document => {
-  //   try {
-  //     const docRef = doc(db, 'data', document);
-  //     const docSnap = await getDoc(docRef);
-
-  //     if (docSnap.exists()) setData(docSnap.data());
-  //     else console.log('No such document!');
-
-  //     setLoading(false);
-  //   } catch (error) {
-  //     console.log(error);
-  //     setError(error);
-  //     setLoading(false);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   console.log(data);
-  // }, [data]);
-
-  // useEffect(() => {
-  //   setLoading(true);
-
-  //   if (netState.isConnected) {
-  //     // console.log('line 67:', netState.isConnected);
-  //     if (isStaticData) {
-  //       fetchStaticData(selection);
-  //     } else if (!isUnique) {
-  //       if (selection == SELECTIONS.BOY) fetchData('boys');
-  //       else if (selection == SELECTIONS.GIRL) fetchData('girls');
-  //     } else {
-  //       if (selection == SELECTIONS.BOY) fetchUniqueData('boys');
-  //       else if (selection == SELECTIONS.GIRL) fetchUniqueData('girls');
-  //     }
-  //   }
-  // }, [selection, language, netState.isConnected]);
+  }, [netState.isConnected, collectionName, document]);
 
   return {data, loading, error, netState};
 };
--- src/screens/RakshiCalculationScreen.js
+++ src/screens/RakshiCalculationScreen.js
@@ -1,324 +1,112 @@
 import {SafeAreaView, View, TextInput, StyleSheet} from 'react-native';
 import {useTranslation} from 'react-i18next';
-import CustomText from '../components/common/CustomText';
-import {LANGUAGES} from '../constants/consts';
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import Details from '../components/Details';
+import useFirebaseData from '../hooks/useFirebaseData';
+import ErrorWrapper from '../components/ErrorWrapper';
 
 const RakshiCalculationScreen = () => {
   const {t, i18n} = useTranslation();
-  const [data, setData] = useState({
+  const [inputString, setInputString] = useState({
     name: '',
     zodiac: '',
     nakshatra: '',
   });
-  // const curLanguage = i18n.language;
+  const {data, loading, error, netState} = useFirebaseData('rashi_nakshatras');
+  const allRashi = useRef([]);
+  const allNakstras = useRef([]);
 
-  // const getNakstraString = val => {
-  //   return val
-  //     .split(',')
-  //     .map(val => t(val.trim()))
-  //     .join(', ');
-  // };
+  function sortAccToLength(a, b) {
+    // Return -1 if a should come before b
+    // Return 0 if a and b are considered equal in order
+    // Return 1 if b should come before a
+    if (a.length > b.length) return -1;
+    else if (a.length == b.length) return 0;
+    else return 1;
+  }
 
-  const calcNakstra = (lowercaseName, rashi) => {
-    let nakshatra = '';
-    const firstThreeLetters = lowercaseName.substring(0, 3);
-    const firstTwoLetters = lowercaseName.substring(0, 2);
-    const firstChar = lowercaseName.charAt(0);
-    if (
-      ['चू', 'चे', 'चो', 'ला', 'chu', 'che', 'cho', 'la'].includes(
-        firstTwoLetters,
-      ) ||
-      ['l'].includes(firstChar)
-    )
-      nakshatra = 'अश्विनी';
-    else if (
-      ['ली', 'लू', 'ले', 'लो', 'li', 'lu', 'le', 'lo'].includes(firstTwoLetters)
-    )
-      nakshatra = 'भरणी';
-    else if (
-      ['aa'].includes(firstTwoLetters) ||
-      ['अ', 'इ', 'उ', 'ए', 'a', 'i', 'u', 'e'].includes(firstChar)
-    )
-      nakshatra = 'कृत्तिका';
-    else if (
-      ['वा', 'वी', 'वू'].includes(firstTwoLetters) ||
-      ['ओ', 'o', 'v', 'w'].includes(firstChar)
-    )
-      nakshatra = 'रोहिणी';
-    else if (
-      ['वे', 'वो', 'का', 'की', 've', 'vo', 'ka', 'ki'].includes(
-        firstTwoLetters,
-      ) ||
-      ['k'].includes(firstChar)
-    )
-      nakshatra = 'मृगशिरा';
-    else if (
-      ['कू', 'ku', 'gh', 'ch'].includes(firstTwoLetters) ||
-      ['घ', 'ङ', 'छ', 'q'].includes(firstChar)
-    )
-      nakshatra = 'आर्द्रा';
-    else if (
-      ['के', 'को', 'ही', 'ke', 'ko', 'ha', 'hi'].includes(firstTwoLetters) ||
-      ['ह', 'h'].includes(firstChar)
-    )
-      nakshatra = 'पुनर्वसु';
-    else if (
-      ['हू', 'हे', 'हो', 'डा', 'hu', 'he', 'ho', 'da'].includes(firstTwoLetters)
-    )
-      nakshatra = 'पुष्य';
-    else if (
-      ['डी', 'डू', 'डे', 'डो'].includes(firstTwoLetters) ||
-      ['d'].includes(firstChar)
-    )
-      nakshatra = 'आश्लेषा';
-    else if (
-      ['मा', 'मी', 'मू', 'मे'].includes(firstTwoLetters) ||
-      ['m'].includes(firstChar)
-    )
-      nakshatra = 'मघा';
-    else if (
-      ['मो', 'टा', 'टी', 'टू', 'mo'].includes(firstTwoLetters) ||
-      ['t'].includes(firstChar)
-    )
-      nakshatra = 'पूर्वाफाल्गुनी';
-    else if (
-      ['टे', 'टो', 'पा', 'पी', 'te', 'to', 'pa', 'pi'].includes(
-        firstTwoLetters,
-      ) ||
-      ['p'].includes(firstChar)
-    )
-      nakshatra = 'उत्तराफाल्गुनी';
-    else if (
-      ['पू', 'pu', 'sh', 'th'].includes(firstTwoLetters) ||
-      ['ष', 'ण', 'ठ'].includes(firstChar)
-    )
-      nakshatra = 'हस्त';
-    else if (
-      ['पे', 'पो', 'रा', 'री', 'pe', 'po', 'ra', 'ri'].includes(
-        firstTwoLetters,
-      ) ||
-      ['r'].includes(firstChar)
-    )
-      nakshatra = 'चित्रा';
-    else if (
-      ['रू', 'रे', 'रो', 'ता', 'ru', 're', 'ro', 'ta'].includes(
-        firstTwoLetters,
-      ) ||
-      ['t'].includes(firstChar)
-    )
-      nakshatra = 'स्वाति';
-    else if (
-      ['ती', 'तू', 'ते', 'तो', 'ti', 'tu', 'te', 'to'].includes(firstTwoLetters)
-    )
-      nakshatra = 'विशाखा';
-    else if (
-      ['ना', 'नी', 'नू', 'ने', 'na', 'ni', 'nu', 'ne'].includes(
-        firstTwoLetters,
-      ) ||
-      ['n'].includes(firstChar)
-    )
-      nakshatra = 'अनुराधा';
-    else if (
-      ['नो', 'या', 'यी', 'यू', 'no', 'ya', 'yi', 'yu'].includes(
-        firstTwoLetters,
-      ) ||
-      ['y'].includes(firstChar)
-    )
-      nakshatra = 'ज्येष्ठा';
-    else if (
-      ['bha', 'bhi'].includes(firstThreeLetters) ||
-      ['ये', 'यो', 'भा', 'भी', 'ye', 'yo'].includes(firstTwoLetters)
-    )
-      nakshatra = 'मूल';
-    else if (
-      ['bhu', 'dha', 'pha'].includes(firstThreeLetters) ||
-      ['भू', 'धा', 'फा', 'ढा'].includes(firstTwoLetters)
-    )
-      nakshatra = 'पूर्वाषाढ़ा';
-    else if (
-      ['bhe', 'bho'].includes(firstThreeLetters) ||
-      ['भे', 'भो', 'जा', 'जी'].includes(firstTwoLetters) ||
-      ['j', 'z', 'x', 'b'].includes(firstChar)
-    )
-      nakshatra = 'उत्तराषाढ़ा';
-    else if (
-      ['kha'].includes(firstThreeLetters) ||
-      ['खी', 'खू', 'खे', 'खो'].includes(firstTwoLetters)
-    )
-      nakshatra = 'श्रवण';
-    else if (
-      ['गा', 'गी', 'गू', 'गे', 'ga', 'gi', 'gu', 'ge'].includes(
-        firstTwoLetters,
-      ) ||
-      ['g'].includes(firstChar)
-    )
-      nakshatra = 'धनिष्ठा';
-    else if (
-      ['गो', 'सा', 'सी', 'सू', 'go'].includes(firstTwoLetters) ||
-      ['s'].includes(firstChar)
-    )
-      nakshatra = 'शतभिषा';
-    else if (
-      ['से', 'सो', 'दा', 'दी', 'se', 'so', 'da', 'di'].includes(
-        firstTwoLetters,
-      ) ||
-      ['d'].includes(firstChar)
-    )
-      nakshatra = 'पूर्वाभाद्रपद';
-    else if (
-      ['tha', 'jha'].includes(firstThreeLetters) ||
-      ['दू', 'du'].includes(firstTwoLetters) ||
-      ['थ', 'झ', 'ञ'].includes(firstChar)
-    )
-      nakshatra = 'उत्तराभाद्रपद';
-    else if (
-      ['cha', 'chi'].includes(firstThreeLetters) ||
-      ['दे', 'दो', 'चा', 'ची', 'de', 'do'].includes(firstTwoLetters) ||
-      ['c'].includes(firstChar)
-    )
-      nakshatra = 'रेवती';
-    else nakshatra = 'Cannot get your nakshatra';
+  useEffect(() => {
+    // console.log('data changed');
+    if (data.length != 0) {
+      allRashi.current = Object.values(data.rashi)
+        .map(val => val.map(alpha => alpha.trim().toLowerCase()))
+        .flat()
+        .sort(sortAccToLength);
+      allNakstras.current = Object.values(data.nakshatras)
+        .map(val => val.map(alpha => alpha.trim().toLowerCase()))
+        .flat()
+        .sort(sortAccToLength);
+    }
+  }, [data]);
 
+  const findNakshatras = input => {
+    let matchedChar = '';
+    for (const alpha of allNakstras.current) {
+      if (alpha === input.substring(0, alpha.length)) {
+        matchedChar = alpha;
+        break;
+      }
+    }
+    let nakshatra = Object.keys(data.nakshatras).filter(val =>
+      data.nakshatras[val].includes(matchedChar),
+    );
     return nakshatra;
   };
 
-  const findRashi = nameVal => {
-    const lowercaseName = nameVal.toLowerCase().trim();
-    const firstThreeLetters = lowercaseName.substring(0, 3);
-    const firstTwoLetters = lowercaseName.substring(0, 2);
-    const firstChar = lowercaseName.charAt(0);
-    let rashi = '';
-
-    if (nameVal.trim() == '') {
-      setData({name: '', zodiac: '', nakshatra: ''});
-      return;
+  const findRashi = input => {
+    let matchedChar = '';
+    for (const alpha of allRashi.current) {
+      if (alpha === input.substring(0, alpha.length)) {
+        matchedChar = alpha;
+        break;
+      }
     }
-
-    if (
-      ['चू', 'चे', 'चो'].includes(firstTwoLetters) ||
-      ['अ', 'आ', 'ल', 'a', 'c', 'l'].includes(firstChar)
-    )
-      rashi = 'Aries';
-    else if (
-      [
-        'इ',
-        'उ',
-        'ए',
-        'ओ',
-        'ई',
-        'ऊ',
-        'ऐ',
-        'औ',
-        'व',
-        'e',
-        'i',
-        'o',
-        'u',
-        'v',
-        'w',
-      ].includes(firstChar)
-    )
-      rashi = 'Taurus';
-    else if (
-      ['gh', 'ch', 'हा'].includes(firstTwoLetters) ||
-      ['घ', 'ङ', 'छ', 'ह', 'क', 'h', 'k', 'q'].includes(firstChar)
-    )
-      rashi = 'Gemini';
-    else if (
-      ['ही', 'हू', 'हे', 'हो', 'hi', 'hu', 'he', 'ho'].includes(
-        firstTwoLetters,
-      ) ||
-      ['ड', 'd'].includes(firstChar)
-    )
-      rashi = 'Cancer';
-    else if (['म', 'ट', 'm', 't'].includes(firstChar)) rashi = 'Leo';
-    else if (
-      ['टो', 'sh', 'th'].includes(firstTwoLetters) ||
-      ['ष', 'ण', 'ठ', 'ढ', 'प', 'p'].includes(firstChar)
-    )
-      rashi = 'Virgo';
-    else if (['र', 'त', 'ऋ', 'r', 't'].includes(firstChar)) rashi = 'Libra';
-    else if (
-      ['तो', 'to'].includes(firstTwoLetters) ||
-      ['न', 'य', 'n', 'y'].includes(firstChar)
-    )
-      rashi = 'Scorpio';
-    else if (
-      ['ये', 'यो', 'ye', 'yo', 'bh', 'dh', 'ph'].includes(firstTwoLetters) ||
-      ['भ', 'ध', 'फ', 'ढ'].includes(firstChar)
-    )
-      rashi = 'Sagittarius';
-    else if (
-      ['bho'].includes(firstThreeLetters) ||
-      ['भो', 'गा', 'गी', 'गि', 'kh'].includes(firstTwoLetters) ||
-      ['ग', 'ज', 'ख', 'ब', 'g', 'j', 'z', 'b', 'x'].includes(firstChar)
-    )
-      rashi = 'Capricorn';
-    else if (
-      ['गू', 'गे', 'गो', 'दा', 'gu', 'ge', 'go', 'da'].includes(
-        firstTwoLetters,
-      ) ||
-      ['स', 'श', 's'].includes(firstChar)
-    )
-      rashi = 'Aquarius';
-    else if (
-      [
-        'दी',
-        'दि',
-        'दू',
-        'दु',
-        'दृ',
-        'दे',
-        'दो',
-        'चा',
-        'ची',
-        'चि',
-        'th',
-        'jh',
-        'ca',
-        'ci',
-      ].includes(firstTwoLetters) ||
-      ['द', 'थ', 'झ', 'ञ', 'च'].includes(firstChar)
-    )
-      rashi = 'Pisces';
-    else {
-      setData({
-        name: nameVal,
-        zodiac: 'Cannot get your zodiac',
-        nakshatra: 'Cannot get your nakshatra',
-      });
-    }
-
-    let nakshatraLocal = calcNakstra(lowercaseName, rashi);
-    setData({name: nameVal, zodiac: rashi, nakshatra: nakshatraLocal});
+    let rashi = Object.keys(data.rashi).filter(val =>
+      data.rashi[val].includes(matchedChar),
+    );
+    return rashi;
   };
 
-  // useEffect(() => {
-  //   console.log(data);
-  // }, [data]);
+  const handleChange = input => {
+    if (input.trim() == '') {
+      setInputString({name: '', zodiac: '', nakshatra: ''});
+      return;
+    }
+    const rashi = findRashi(input.trim().toLowerCase());
+    const nakshatra = findNakshatras(input.trim().toLowerCase());
+    setInputString({
+      name: input,
+      zodiac:
+        rashi.length == 0 ? 'Cannot get your zodiac' : rashi[0].toLowerCase(),
+      nakshatra:
+        nakshatra.length == 0
+          ? 'Cannot get your nakshatra'
+          : nakshatra[0].toLowerCase(),
+    });
+  };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <TextInput
-        style={styles.searchBox}
-        value={data.name}
-        onChangeText={findRashi}
-        placeholder={t('Enter Name')}
-        placeholderTextColor="#dfe0e8"
-      />
-      <Details
-        label={t('Zodiac')}
-        data={t(data.zodiac)}
-        customStyle={{marginHorizontal: 20}}
-      />
-      <Details
-        label={t('Nakshatra')}
-        data={t(data.nakshatra)}
-        customStyle={{marginHorizontal: 20}}
-      />
-    </SafeAreaView>
+    <ErrorWrapper loading={loading} error={error} netState={netState}>
+      <SafeAreaView style={styles.container}>
+        <TextInput
+          style={styles.searchBox}
+          value={inputString.name}
+          onChangeText={handleChange}
+          placeholder={t('Enter Name')}
+          placeholderTextColor="#dfe0e8"
+        />
+        <Details
+          label={t('Zodiac')}
+          data={t(inputString.zodiac)}
+          customStyle={{marginHorizontal: 20}}
+        />
+        <Details
+          label={t('Nakshatra')}
+          data={t(inputString.nakshatra)}
+          customStyle={{marginHorizontal: 20}}
+        />
+      </SafeAreaView>
+    </ErrorWrapper>
   );
 };
 
