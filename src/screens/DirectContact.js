981569ba6ee9d2b39864394a097f560beb719810
Aarjav Jain <aarjavjain2309@gmail.com>
2024-02-15 20:13:39 +0530

    added firebase to direct contact

--- src/screens/DirectContact.js
+++ src/screens/DirectContact.js
@@ -5,19 +5,32 @@ import {useTranslation} from 'react-i18next';
 import PhoneNo from '../components/PhoneNo';
 import useFirebaseData from '../hooks/useFirebaseData';
 import ErrorWrapper from '../components/ErrorWrapper';
+import {LANGUAGES} from '../constants/consts';
 
 export default function DirectContact() {
-  const {t} = useTranslation();
-  const {data, loading, error, netState} = useFirebaseData('data', 'Phone Nos');
+  const {t, i18n} = useTranslation();
+  const {data, loading, error, netState} = useFirebaseData(
+    'data',
+    'Direct Contact',
+  );
+
+  const curLanguage =
+    i18n.language == LANGUAGES.ENGLISH.key
+      ? LANGUAGES.ENGLISH.label
+      : LANGUAGES.HINDI.label;
 
   return (
     <ErrorWrapper loading={loading} error={error} netState={netState}>
       <SafeAreaView style={styles.container}>
-        <CustomText text={t('Direct Contact Msg')} size={30} />
-        <View style={styles.whtsapDiv}>
-          {Object.values(data).map((phoneNo, index) => (
-            <PhoneNo key={index} number={phoneNo} />
+        {data.length != 0 &&
+          data[curLanguage].map((point, index) => (
+            <CustomText key={index} text={`${point}`} size={28} />
           ))}
+        <View style={styles.whtsapDiv}>
+          {data.length != 0 &&
+            Object.values(data?.PhoneNos).map((phoneNo, index) => (
+              <PhoneNo key={index} number={phoneNo} />
+            ))}
         </View>
       </SafeAreaView>
     </ErrorWrapper>
